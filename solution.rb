ac1ec7a1457296d10c9d01f2de5c29f06e740699
dguilar1994 <andrea@MacBook-Pro-de-Andrea.local>
2017-05-24 00:22:08 -0500

    reto 3 1st try

--- solution.rb
+++ solution.rb
@@ -1,10 +1,23 @@
 require 'sinatra'
 
+#RETO 1
 #get '/' do
 #   @nombre = params[:nombre]
 #   erb :index
 #end
 
-get "/makers/:nombre" do
-  "Hola #{params[:nombre].capitalize}!"
+#RETO 2
+#get "/makers/:nombre" do
+#  "Hola #{params[:nombre].capitalize}!"
+#end
+
+#RETO3
+
+get '/' do
+erb  :index
+end
+
+post '/saluda' do
+@nombre = params[:nombre]
+"¡Hola #{@nombre}!"
 end
